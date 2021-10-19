66d052204422751aed2636be733d5ce2c327e07b
danielRsantana99 <danielrsantana9975@gmail.com>
2021-10-19 19:02:39 -0300

    teste2

--- Atividade III/codigo/resources/views/clientes/index.blade.php
+++
@@ -1,8 +0,0 @@
-<h1>Pagina de clientes</h1>
-
-@foreach ($clientes as $cliente)
-	nome do cliente: $cliente->nome;
-	debito do cliente: $cliente->debito;
-	descrição: $cliente->descricao;
-
-@endforeach
--- Atividade III/codigo/resources/views/fornecedores/index.blade.php
+++
@@ -1,8 +0,0 @@
-<h1>Pagina de clientes</h1>
-
-@foreach ($Fornecedores as $Fornecedores)
-	Nome do fornecedor: $Fornecedores->nome;
-	CNPJ do fornecedor: $Fornecedores->cnpj;
-	endereço: $Fornecedores->endereco;
-
-@endforeach
