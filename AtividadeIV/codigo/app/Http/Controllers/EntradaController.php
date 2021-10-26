5958a40a186a1b22bb721c18140691f156ed977c
danielRsantana99 <danielrsantana9975@gmail.com>
2021-10-26 00:25:27 -0300

    zxcz

--- AtividadeIV/codigo/app/Http/Controllers/EntradaController.php
+++ AtividadeIV/codigo/app/Http/Controllers/EntradaController.php
@@ -13,21 +13,17 @@ class EntradaController extends Controller
      
 		return view('entradas.index', ['entradas' =>$entradas]);
 	}
-
 	public function create(){
 		
      
 		return view('entradas.create');
 	}
-
-
 	public function store(Request $resquest){
 		$entrada - new Entrada();
 		$entrada->idfornecedor = $resquest->idfornecedor;
 		$entrada->valortotal = $resquest->valortotal;
 		$entrada->save();
 	}
-
 	public function show($id){
 		if($id){
 			$entrada = Entrada::where('id',$id)->get();
@@ -36,17 +32,14 @@ class EntradaController extends Controller
 		}
 		return view('entradas.show',['entradas'=>$entrada]);
 	}
-
 	public function edit($id){
 		$cliente = Entrada::find($id);
 		return view('entradas.edit',['entradas'=>$entrada]);
 	}
-
 	public function update(Request $resquest){
 		Entrada::find($resquest->id)->update($resquest->except('_method'));
 		return redirect('entradas/index')->with('msg','Cadastro realizado com sucesso');
 	}	
-
 	public function destroy($id){
 		Entrada::findOrFail($id)->delete();
 		return redirect('entradas/index')->with('msg','entrada apagado com sucesso');
