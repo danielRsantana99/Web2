88cad2475d968fc41ca598333159e4c64a70d064
danielRsantana99 <danielrsantana9975@gmail.com>
2021-11-01 23:54:24 -0300

    atividade V

---
+++ atividade V/codigo/layouts/grid.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale =1.">
+	<!-- CSS only -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+	<!-- CSS local -->
+	<link rel="stylesheet" type="text/css" href="./css/grids.css">
+	<title>GRID</title>
+</head>
+<body>
+	<div>
+		<h1>Pagina com Grid</h1>
+	</div>
+    
+    <p>Tabelas banco de Dados</p>
+
+    <div class="container">
+        <div>Clientes</div>
+        <div>Fornecedores</div>
+        <div>Entradas</div>
+        <div>Itensentradas</div>
+        <div>Itensvendas</div>
+        <div>Vendas</div>
+        <div>Produtos</div>
+    </div>
+	<!--JavaScript Bundle with Popper -->
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+</body>
+</html>
---
+++ atividade V/codigo/layouts/normalflow.blade.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale =1.">
+	<!-- CSS only -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+	<!-- CSS local -->
+	<link rel="stylesheet" type="text/css" href="./css/normalflow.css">
+	<title>NORMAL FLOW</title>
+</head>
+<body>
+	<div class="row" id="titulo">
+		<h1>Pagina com Normal Flow</h1>
+	</div>
+	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+	  <div class="container-fluid">
+	    <a class="navbar-brand" href="#">NormalFlow</a>
+	    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+	      <span class="navbar-toggler-icon"></span>
+	    </button>
+	    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+	      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+	        <li class="nav-item">
+	          <a class="nav-link active" aria-current="page" href="#">Home</a>
+	        </li>
+	        <li class="nav-item">
+	          <a class="nav-link" href="#">Clientes</a>
+	        </li>
+	        <li class="nav-item">
+	          <a class="nav-link" href="#">Fornecedores</a>
+	        </li>
+	        <li class="nav-item">
+	          <a class="nav-link" href="#">Entrada</a>
+	        </li>
+	        <li class="nav-item">
+	          <a class="nav-link" href="#">Vendas</a>
+	        </li>
+	      </ul>
+	      <form class="d-flex">
+	        <input class="form-control me-2" type="search" placeholder="pesquisar" aria-label="Search">
+	        <button class="btn btn-outline-success" type="submit">Pesquisar</button>
+	      </form>
+	    </div>
+	  </div>
+	</nav>
+	<div>
+		<p>Descrição: sistema de vendas feito como um projeto laravel</p>
+
+		<p>O sistema inclui banco de dados,Crud dentro do sistema , Telas com bootstrap, niveis de acesso, backup de dados e muitas outras funções</p>
+		<p>Todos os dados salvos são criptografados permitindo apenas quem tem a chave de acesso obte-las</p>
+	</div>
+
+	<!-- JavaScript Bundle with Popper -->
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+</body>
+</html>
